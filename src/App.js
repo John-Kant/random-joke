d62c20ca6742e82ee6cf727652210f2af1e2b126
John Gomez <johngomez5541@yahoo.com>
2021-01-04 10:55:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from "axios"
 import './App.css';
 
 function App() {
+  const [randomJoke, setRandomJoke] = useState("");
+
+  const getJoke = () => {
+    axios.get("https://official-joke-api.appspot.com/random_joke").then(
+            (response) => {
+              setRandomJoke(response.data.setup + "... :)" + response.data.punchline);
+            }
+        )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Get random Jokes to make your day!</h2>
+      <h1>{randomJoke}</h1>
+      <button className="btn" onClick={getJoke}>Click for a Joke</button>
     </div>
   );
 }
